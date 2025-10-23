df56bf99835a657e888b806db1b78ee59d886e09
Julio Cezar mattos <julio.mattos1993@gmail.com>
2025-10-23 23:42:48 +0000

    Habilidades especiais e áreas de efeito

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,62 +1,167 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define N 10
 
 int main() {
-    int tabuleiro[10][10];
+    int tab[N][N];
+    int navioAfetado[N][N];
     int i, j;
 
-    // Inicializa o tabuleiro com 0 (água)
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j < 10; j++) {
-            tabuleiro[i][j] = 0;
+    // Inicializar tabuleiro
+    for(i = 0; i < N; i++){
+        for(j = 0; j < N; j++){
+            tab[i][j] = 0; // água
+            navioAfetado[i][j] = 0;
         }
     }
 
-    // Coordenadas dos navios
-    // Dois horizontais/verticais + dois diagonais
-
     // Navio horizontal
-    int linhaH = 1, colH = 2;
-    for (j = 0; j < 3; j++) {
-        if (colH + j < 10)
-            tabuleiro[linhaH][colH + j] = 3;
+    for(j = 2; j < 5; j++){
+        tab[1][j] = 3;
     }
 
     // Navio vertical
-    int linhaV = 5, colV = 7;
-    for (i = 0; i < 3; i++) {
-        if (linhaV + i < 10)
-            tabuleiro[linhaV + i][colV] = 3;
+    for(i = 5; i < 8; i++){
+        tab[i][7] = 3;
     }
 
-    // Navio diagonal principal (↘)
-    int linhaD1 = 3, colD1 = 3;
-    for (i = 0; i < 3; i++) {
-        if (linhaD1 + i < 10 && colD1 + i < 10)
-            if (tabuleiro[linhaD1 + i][colD1 + i] == 0)
-                tabuleiro[linhaD1 + i][colD1 + i] = 3;
+    // Navio diagonal ↘
+    for(i = 0; i < 3; i++){
+        tab[3 + i][3 + i] = 3;
     }
 
-    // Navio diagonal secundária (↙)
-    int linhaD2 = 2, colD2 = 8;
-    for (i = 0; i < 3; i++) {
-        if (linhaD2 + i < 10 && colD2 - i >= 0)
-            if (tabuleiro[linhaD2 + i][colD2 - i] == 0)
-                tabuleiro[linhaD2 + i][colD2 - i] = 3;
+    // Navio diagonal ↙
+    for(i = 0; i < 3; i++){
+        tab[2 + i][8 - i] = 3;
     }
 
-    // Exibe o tabuleiro
+    // MATRIZES DE HABILIDADES (5x5)
+    int cone[5][5], cruz[5][5], oct[5][5];
+    int meio = 2;
+
+    // CONE
+    for(i = 0; i < 5; i++){
+        for(j = 0; j < 5; j++){
+            if(abs(j - meio) <= i)
+                cone[i][j] = 1;
+            else
+                cone[i][j] = 0;
+        }
+    }
+
+    // CRUZ
+    for(i = 0; i < 5; i++){
+        for(j = 0; j < 5; j++){
+            if(i == meio || j == meio)
+                cruz[i][j] = 1;
+            else
+                cruz[i][j] = 0;
+        }
+    }
+
+    // OCTAEDRO
+    for(i = 0; i < 5; i++){
+        for(j = 0; j < 5; j++){
+            if(abs(i - meio) + abs(j - meio) <= meio)
+                oct[i][j] = 1;
+            else
+                oct[i][j] = 0;
+        }
+    }
+
+    // POSIÇÕES DAS HABILIDADES NO TABULEIRO
+    int coneL = 0, coneC = 4;
+    int cruzL = 4, cruzC = 2;
+    int octL = 7, octC = 6;
+
+    // APLICAR CONE
+    for(i = 0; i < 5; i++){
+        for(j = 0; j < 5; j++){
+            if(cone[i][j] == 1){
+                int l = coneL + i;
+                int c = coneC - 2 + j;
+                if(l >= 0 && l < N && c >= 0 && c < N){
+                    if(tab[l][c] == 3)
+                        navioAfetado[l][c] = 1;
+                    else
+                        tab[l][c] = 5;
+                }
+            }
+        }
+    }
+
+    // APLICAR CRUZ
+    for(i = 0; i < 5; i++){
+        for(j = 0; j < 5; j++){
+            if(cruz[i][j] == 1){
+                int l = cruzL - 2 + i;
+                int c = cruzC - 2 + j;
+                if(l >= 0 && l < N && c >= 0 && c < N){
+                    if(tab[l][c] == 3)
+                        navioAfetado[l][c] = 1;
+                    else
+                        tab[l][c] = 5;
+                }
+            }
+        }
+    }
+
+    // APLICAR OCTAEDRO
+    for(i = 0; i < 5; i++){
+        for(j = 0; j < 5; j++){
+            if(oct[i][j] == 1){
+                int l = octL - 2 + i;
+                int c = octC - 2 + j;
+                if(l >= 0 && l < N && c >= 0 && c < N){
+                    if(tab[l][c] == 3)
+                        navioAfetado[l][c] = 1;
+                    else
+                        tab[l][c] = 5;
+                }
+            }
+        }
+    }
+
+    // IMPRIMIR TABULEIRO
     printf("\n     A  B  C  D  E  F  G  H  I  J\n");
     printf("   ---------------------------------\n");
-
-    for (i = 0; i < 10; i++) {
+    for(i = 0; i < N; i++){
         printf("%2d | ", i + 1);
-        for (j = 0; j < 10; j++) {
-            printf("%d  ", tabuleiro[i][j]);
+        for(j = 0; j < N; j++){
+            if(navioAfetado[i][j] == 1)
+                printf("X  ");
+            else
+                printf("%d  ", tab[i][j]);
+        }
+        printf("\n");
+    }
+    printf("   ---------------------------------\n");
+
+    // MATRIZES (opcional)
+    printf("\nMatriz CONE:\n");
+    for(i = 0; i < 5; i++){
+        for(j = 0; j < 5; j++){
+            printf("%d ", cone[i][j]);
         }
         printf("\n");
     }
 
-    printf("   ---------------------------------\n");
+    printf("\nMatriz CRUZ:\n");
+    for(i = 0; i < 5; i++){
+        for(j = 0; j < 5; j++){
+            printf("%d ", cruz[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nMatriz OCTAEDRO:\n");
+    for(i = 0; i < 5; i++){
+        for(j = 0; j < 5; j++){
+            printf("%d ", oct[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
